991ea47e83f464f611196085d09334d2c1a6607e
huanghuilai <1175215747@qq.com>
2023-12-17 22:33:10 +0800

    调整

--- src/service/api/auth.ts
+++ src/service/api/auth.ts
@@ -45,7 +45,7 @@ export function fetchUserInfo() {
  * @description 后端根据用户id查询到对应的角色类型，并将路由筛选出对应角色的路由数据返回前端
  */
 export function fetchUserRoutes(userId: string) {
-  return request.post<ApiRoute.Route>('/getUserRoutes', { userId });
+  return request.get<ApiRoute.Route>('/bosen-admin/role/menu/listMenuAfterLogin?roleId=' + userId);
 }
 
 /**
